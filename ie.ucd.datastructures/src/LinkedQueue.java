8dae36189d2f44a2cc05f03e5053ceaf10e956dc
Mattodonnello <48354237+Mattodonnello@users.noreply.github.com>
2021-04-08 16:03:37 +0100

    Add code implementation for LinkedQueue
    This includes code for our enqueue and dequeue functions

--- ie.ucd.datastructures/src/LinkedQueue.java
+++ ie.ucd.datastructures/src/LinkedQueue.java
@@ -1,38 +1,95 @@
 public class LinkedQueue<E> implements Queue<E> {
+    private Node<E> head = null; // head node of the list (or null if empty)
+    private Node<E> last = null;
+    private int size;
+	
+    private static class Node<E> {
+    	private E element;  // reference to the element stored at this node
+    	private Node<E> next; // reference to the subsequent node in the list  
+		public Node<E> prev;
+    	public Node(E e, Node<E> n) {
+    	    if (e == null) throw new NullPointerException();
+    		element = e;
+    		next = n;  
+    		}
+    	Node(){}
+    }
+    
+    public LinkedQueue() {
+        head = null;
+        last  = null;
+        size = 0;
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LinkedQueue<Integer> q = new LinkedQueue<Integer>();
+		q.enqueue(1);
+		q.enqueue(2);
+		q.enqueue(3);
+		q.enqueue(4);
+		q.enqueue(5);
+		System.out.println(q);
 	}
 
+	public String toString() {
+		Node <E> curr = head;
+        StringBuilder sb = new StringBuilder();
+        sb.append("["); 
+        while(curr.next!=null) {
+        	sb.append(curr.element);
+        	sb.append(", ");
+        	curr=curr.next;
+        }
+        if(curr.next==null && curr!=null) {
+        	sb.append(curr.element);
+        }
+        sb.append("]");
+        return sb.toString();
+	}
+	
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+	return size;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return head == null;
 	}
 
 	@Override
 	public void enqueue(E e) {
-		// TODO Auto-generated method stub
-		
+		Node oldlast = last;
+        last = new Node(e, null);
+        if (isEmpty()) {
+        	head = last;
+        }
+        else {
+        	oldlast.next = last;
+        }
+        size++;
 	}
 
 	@Override
 	public E first() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) {
+			return null;
+		}
+		else return head.element;
 	}
 
 	@Override
-	public E dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+	public E dequeue() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Queue is empty, nothing can be removed");
+		}
+        E e = head.element;
+        head = head.next;
+        size--;
+        if (isEmpty()) {
+        	last = null;  
+        }
+        return e;
 	}
 
 }
